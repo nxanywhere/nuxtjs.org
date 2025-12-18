8be195a0d3d058c44958e3559e06c68fb95f9967
Sébastien Chopin <atinux@gmail.com>
2025-12-18 13:06:10 +0100

    chore: update banner link

--- components/theme/app/AppBanner.vue
+++ components/theme/app/AppBanner.vue
@@ -2,7 +2,7 @@
   <div class="group flex items-center delay-200 h-10 bg-primary text-black">
     <p class="sm:top-1.5 w-full pl-4 sm:pl-0 sm:text-center text-sm font-medium">
       You are browsing Nuxt 2 docs. Go to <a href="https://nuxt.com" class="font-bold hover:underline">Nuxt 3 docs</a>,
-      or learn more about <nuxt-link to="/lts" class="font-bold hover:underline">Nuxt 2 Long Term Support</nuxt-link>.
+      or learn more about <a href="https://www.herodevs.com/support/nuxt-nes?utm_source=nuxtjs&utm_medium=affiliate&utm_campaign=nuxt2eol&utm_content=link" class="font-bold hover:underline" target="_blank" rel="noopener">Nuxt 2 Long Term Support</a>.
     </p>
   </div>
 </template>
